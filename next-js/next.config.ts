f277a2397b60717ba72a4942dea0f4b9c481dcd3
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-05-24 16:20:58 +0530

    Add allowedDevOrigins to Next.js configuration

--- next-js/next.config.ts
+++ next-js/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['*.worker.buildx.website']
 };
 
 export default nextConfig;
